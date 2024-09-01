68161a40415717fd8b25162efdb437749e06cb3a
Jawad Sher <jawadsher04@gmail.com>
2024-09-01 15:43:22 +0500

    adding react routing feature

--- 01 - Gemini Clone Project/src/main.jsx
+++ 01 - Gemini Clone Project/src/main.jsx
@@ -1,12 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import ContextProvider from './context/Context.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:'/', 
+    element:<App />
+},
+])
 
 createRoot(document.getElementById('root')).render(
   <ContextProvider>
-    <App />
+    <RouterProvider router={router}/>
   </ContextProvider>
  
 )
